2ba196f1335106e6c4022a510ad5ee13d53aef23
Mark Marada <maradamark99@gmail.com>
2022-02-25 11:24:30 +0100

    Timer + styling

--- src/app/content.component.ts
+++ src/app/content.component.ts
@@ -12,26 +12,35 @@ export class ContentComponent implements OnInit, OnDestroy {
     numOfCorrect: number = 0;
     index: number = 0;
     input: string = "";
-    words: string[] = ["hurl", "snub", "circle", "banish", "squash", "preparation", "shop",
+    /*words: string[] = ["hurl", "snub", "circle", "banish", "squash", "preparation", "shop",
         "rack", "understand", "cultural", "achieve", "speed", "play", "regular", "nose", "dedicate", "spontaneous",
         "depart", "favor", "chew", "cream", "need", "illness", "exit", "volunteer", "pupil", "killer", "motorist",
         "describe", "ask", "adoption", "cigarette", "affair", "transform", "cord", "shame", "studio", "twist",
         "reluctance", "bet", "manner", "trouble", "shine", "bolt", "racism", "engagement", "lake", "public", "joystick", "flash"];
-    
-    //TODO: create a service class for it
-
+    */
+    //TODO: create a service class for this
+    words: string[] = [];
     url = 'https://random-word-api.herokuapp.com/word?number=200';
     subscription?: Subscription;
+    timer: number = 60;
 
     constructor(private http: HttpClient) { }
     getWords() {
-        //return this.http.get<string[]>(this.url);
+        return this.http.get<string[]>(this.url);
     }
+
     ngOnInit() {
-        //this.subscription = this.getWords().subscribe(data => (this.words = data));
+        this.subscription = this.getWords().subscribe(data => (this.words = data));
+        setInterval(() => this.decreaseTime(), 1000);
     }
-    ngOnDestroy(): void {
-        //this.subscription?.unsubscribe();
+
+    ngOnDestroy(){
+        this.subscription?.unsubscribe();
+    }
+
+    decreaseTime(){
+        if(this.timer > 0)
+            this.timer--;
     }
 
     checkInput(){
