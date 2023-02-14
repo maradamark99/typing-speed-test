33aee7cd523a175f8fe55fff2fd5f6f4c028fcf4
maradamark99 <maradamark99@gmail.com>
2023-02-14 20:23:45 +0100

    feat: add an input field

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,17 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-	public readonly words?: Array<String> 
+  public readonly words?: Array<String>;
+  charIndex?: number;
+  wordIndex?: number;
+  test?: boolean
 
 	constructor() { 
 		// for testing purposes
     this.words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "Lorem",
       "ipsum", "dolor", "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor",
-      "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur",]
+      "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur",];
 	}
 
   ngOnInit(): void {
+    this.charIndex = 0;
+    this.wordIndex = 0;
+  }
+
+  checkInput(value: string): void {
+    this.test = value.toLowerCase() == this.words![this.wordIndex!][this.charIndex!].toLowerCase();
   }
 
 }
