561950ad44d42a9430db2742400e0bc3bb990053
maradamark99 <maradamark99@gmail.com>
2023-02-15 16:46:47 +0100

    feat: add previous words

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,25 +6,47 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public readonly words?: Array<String>;
+  private originalWords?: Array<String>;
+  public currentWords?: Array<String>;
+  public prevWords?: Array<String>;
   charIndex?: number;
   wordIndex?: number;
-  test?: boolean
+  test?: boolean;
 
 	constructor() { 
-		// for testing purposes
-    this.words = ["Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "Lorem",
-      "ipsum", "dolor", "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor",
-      "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur",];
 	}
 
   ngOnInit(): void {
+		// for testing purposes
+    this.originalWords = ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "lorem",
+      "ipsum", "dolor", "sit", "amet", "consectetur", "Lorem", "ipsum", "dolor",
+      "sit", "amet", "consectetur", "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",];
+    this.currentWords = [...this.originalWords];
     this.charIndex = 0;
     this.wordIndex = 0;
+    this.prevWords = []
+    this.prevWords![this.wordIndex] = "";
+
   }
 
-  checkInput(value: string): void {
-    this.test = value.toLowerCase() == this.words![this.wordIndex!][this.charIndex!].toLowerCase();
+  checkInput(value: KeyboardEvent): void {
+    if (value.key.trim().length < 1)
+      return
+    this.test = value.key == this.originalWords![this.wordIndex!][this.charIndex!];
+    this.prevWords![this.wordIndex!] += value.key
+    this.currentWords![this.wordIndex!] = this.currentWords![this.wordIndex!].substring(1);
+    if(this.charIndex! < this.originalWords![this.wordIndex!].length-1)
+      this.charIndex!++;
+    this.test = undefined;
+  }
+
+  nextWord(value: string): void {
+    if (value.trim().length < 1)
+      return;
+    this.currentWords![this.wordIndex!] = "";
+    this.charIndex = 0;
+    this.wordIndex!++;
+    this.prevWords![this.wordIndex!] = "";
   }
 
 }
