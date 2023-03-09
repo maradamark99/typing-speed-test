ff43558783f04046bd1b499d2b9c0a35f82d2466
maradamark99 <maradamark99@gmail.com>
2023-03-09 19:36:31 +0100

    feat: use the form component inside registration

---
+++ src/app/auth/registration/registration.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IUser } from 'src/app/interfaces/user.interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { CustomValidator } from 'src/app/utils/custom-validator';
+import { IFormControlDetails } from '../../interfaces/form-control-details';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss'],
+})
+export class RegistrationComponent implements OnInit {
+
+  public readonly formControlDetails?: IFormControlDetails[] = [
+    {
+      name: 'email',
+      placeholder: 'Email',
+      type: 'email',
+      validators: [Validators.required, Validators.email, Validators.maxLength(255)],
+    },
+    {
+      name: 'username',
+      placeholder: 'Username',
+      type: 'text',
+      validators: [Validators.minLength(4), Validators.maxLength(255), CustomValidator.usernameValidator()],
+    },
+    {
+      name: 'password',
+      placeholder: 'Password',
+      type: 'password',
+      validators: [
+        Validators.minLength(8),
+        Validators.maxLength(255),
+        CustomValidator.passwordStrengthValidator()
+      ],
+    },
+    {
+      name: 'passwordConfirmation',
+      placeholder: 'Password confirmation',
+      type: 'password',
+      validators: [Validators.required],
+      saveValue: false
+    },
+  ];
+  public readonly formValidators: CustomValidator[] =
+    [CustomValidator.matchingControlValuesValidator('password', 'passwordConfirmation')];
+
+  ngOnInit() {
+
+  }
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  onUserSubmitted(user: IUser) {
+    this.authService.registerUser(user).subscribe((result) => console.log(result));
+    this.redirect();
+  }
+
+  redirect() {
+    this.router.navigate(['/login']);
+  }
+
+}
