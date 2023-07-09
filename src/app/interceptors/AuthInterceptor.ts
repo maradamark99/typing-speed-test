b9cef2930bc7c2404bc9637e60c9433b786d408e
maradamark99 <maradamark99@gmail.com>
2023-07-09 21:26:10 +0200

    chore: format code

--- src/app/interceptors/AuthInterceptor.ts
+++ src/app/interceptors/AuthInterceptor.ts
@@ -3,24 +3,24 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { TokenService } from "../services/token.service";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class AuthInterceptor implements HttpInterceptor {
 
-    private readonly TOKEN_PREFIX = "Bearer ";
+  private readonly TOKEN_PREFIX = "Bearer ";
 
-    constructor(private readonly tokenService: TokenService) { }
+  constructor(private readonly tokenService: TokenService) { }
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = this.tokenService.getToken();
-        if (token && this.tokenService.isValidToken(token)) {
-            const cloned = req.clone({
-                headers: req.headers.set("Authorization",
-                    this.TOKEN_PREFIX + token)
-            });
-            return next.handle(cloned);
-        }
-        return next.handle(req);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.tokenService.getToken();
+    if (token && this.tokenService.isValidToken(token)) {
+        const cloned = req.clone({
+            headers: req.headers.set("Authorization",
+                this.TOKEN_PREFIX + token)
+        });
+        return next.handle(cloned);
     }
+    return next.handle(req);
+  }
 
 }
     
