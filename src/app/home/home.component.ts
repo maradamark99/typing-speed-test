ca97f79491a79513ecd9671d3fa341a27787aae5
maradamark99 <maradamark99@gmail.com>
2023-07-14 19:59:25 +0200

    feat: create difficulty interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,31 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TimerService } from '../services/timer.service';
 import { WordService } from '../services/word.service';
+import { DifficultyService } from '../services/difficulty.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   readonly countFrom: number = 60;
-  currentTime?: number; 
+  currentTime: number = this.countFrom;
+  difficulties: string[] = [];
   timerSubscription: Subscription = Subscription.EMPTY;
+  selectedDifficulty?: string;
 
-  constructor(public readonly wordService: WordService, public readonly timerService: TimerService) { 
+  constructor(private readonly difficultyService: DifficultyService, public readonly wordService: WordService, public readonly timerService: TimerService) { 
+  }
+  ngOnInit(): void {
+    this.difficultyService.getAll().subscribe({
+      error: (e) => console.log(e),
+      next: (result) => {
+        this.difficulties = result.map((d) => d.value);
+        this.selectedDifficulty = this.difficulties[0];
+      }
+    })
   }
 
   handleIsFinished(value: boolean) {
