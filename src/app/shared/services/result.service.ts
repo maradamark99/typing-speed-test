f3a0a3854e38770bf56b2b94b1e9f0f65ae0627c
maradamark99 <maradamark99@gmail.com>
2023-07-23 20:09:09 +0200

    refactor: use paginationutil to process pagination params

--- src/app/shared/services/result.service.ts
+++ src/app/shared/services/result.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ApiPath } from '../utils/api-path';
@@ -6,6 +6,8 @@ import ResultRequest from '../interfaces/result-request';
 import { ResultResponse } from '../interfaces/result-response';
 import { Observable, retry } from 'rxjs';
 import { Page } from '../interfaces/page';
+import { PageOptions } from '../interfaces/page-options';
+import { processPageOptionParams } from '../utils/pagination-util';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +16,9 @@ export class ResultService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(page: number, size: number): Observable<Page<ResultResponse>> {
-    return this.http.get<Page<ResultResponse>>(environment.apiUrl + ApiPath.RESULTS, { params: { page: page, size: size } }).pipe(retry(3));
+  getAll(pageOptions: PageOptions): Observable<Page<ResultResponse>> {
+    const params = processPageOptionParams(pageOptions);
+    return this.http.get<Page<ResultResponse>>(environment.apiUrl + ApiPath.RESULTS, { params }).pipe(retry(3));
   }
 
   save(result: ResultRequest) {
