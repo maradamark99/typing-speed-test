d4b88117f3ed76e49bb8beb1a1e4d9b11015dcc6
maradamark99 <maradamark99@gmail.com>
2023-07-24 20:17:17 +0200

    feat: use a context to keep track of indexes

--- src/app/shared/components/sort-header/sort-header.component.ts
+++ src/app/shared/components/sort-header/sort-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Sort, SortDirection } from '../../interfaces/sort';
+import SortHeaderContext from './sort-header-context';
 
 @Component({
   selector: 'app-sort-header',
@@ -9,12 +10,12 @@ import { Sort, SortDirection } from '../../interfaces/sort';
 export class SortHeaderComponent implements OnInit {
   @Input() column?: string;
   @Input() sortField?: string;
-  @Output() sortChanged: EventEmitter<Sort> = new EventEmitter();
   @Input() sortOrder = [SortDirection.DEFAULT, SortDirection.ASC, SortDirection.DESC]
-  private index: number = 0;
+  @Input() context?: SortHeaderContext;
+  @Output() sortChanged: EventEmitter<Sort> = new EventEmitter();
 
   get currentSortOrder(): SortDirection {
-    return this.sortOrder[this.index];
+    return this.sortOrder[this.context!.get(this.column!)!];
   }
 
   get sortDirection(): typeof SortDirection {
@@ -29,13 +30,14 @@ export class SortHeaderComponent implements OnInit {
   }
 
   handleClick() {
-    if (this.index == this.sortOrder.length - 1) {
-      this.index = 0;
+    this.context!.set(this.column!, this.context!.get(this.column!) + 1);
+    const currentIndex = this.context!.get(this.column!);
+    if (this.sortOrder[currentIndex] != SortDirection.DEFAULT) {
+      this.sortChanged.emit({ field: this.sortField ?? this.column!, direction: this.sortOrder[currentIndex] });
     }
-    if (this.sortOrder[this.index] != SortDirection.DEFAULT) {
-      this.sortChanged.emit({field: this.sortField ?? this.column!, direction: this.sortOrder[this.index]});
-    }
-    this.index++;
+    if (currentIndex == this.sortOrder.length) {
+      this.context!.set(this.column!, 0);
+    }    
   }
 
 }
