6ca6d075fa05637cc6d847af9a762d3172d0d16f
maradamark99 <maradamark99@gmail.com>
2023-08-08 21:41:17 +0200

    refactor: use default values when page and/or size is null

--- src/app/shared/utils/pagination-util.ts
+++ src/app/shared/utils/pagination-util.ts
@@ -1,19 +1,26 @@
 import { PageOptions } from "../interfaces/page-options";
 import { HttpParams } from "@angular/common/http";
 
+export default class PaginationUtil {
+  private static DEFAULT_PAGE = 0;
+  private static DEFAULT_SIZE = 10;
 
-export function processPageOptionParams(pageOptions: PageOptions): HttpParams {
-  let params = new HttpParams();
-  params = params.append('page', pageOptions.page);
-  params = params.append('size', pageOptions.size);
-  if (pageOptions.sort) {
-    pageOptions.sort.forEach((value) => {
-      if (value.direction && value.field) {
-        params = params.append('sort', `${value.field},${value.direction}`);
-      }
-    }); 
+  static processPageOptionParams(pageOptions: Partial<PageOptions>): HttpParams {
+    let params = new HttpParams();
+    params = params.append('page', pageOptions.page ?? this.DEFAULT_PAGE);
+    params = params.append('size', pageOptions.size ?? this.DEFAULT_SIZE);
+
+    if (pageOptions.sort) {
+      pageOptions.sort.forEach((value) => {
+        if (value.direction && value.field) {
+          params = params.append('sort', `${value.field},${value.direction}`);
+        }
+      }); 
+    }
+    return params;
   }
-  return params;
 }
+
+
 
 
