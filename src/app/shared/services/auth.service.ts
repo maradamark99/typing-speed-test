770e7d41dc189ae0de5ce8aae7980f3425a9ef35
maradamark99 <maradamark99@gmail.com>
2023-08-08 21:54:34 +0200

    feat: create methods for logout, isAuthentication and hasRole check

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -4,19 +4,19 @@ import { environment } from 'src/environments/environment';
 import { User } from '../interfaces/user';
 import { ApiPath } from '../utils/api-path';
 import { Observable } from 'rxjs';
+import { TokenService } from './token.service';
 
 type LoginResponse = {
   token: string;
 }
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-
   constructor(
     private readonly http: HttpClient,
+    private readonly tokenService: TokenService,
   ) { }
 
   register(user: User): Observable<void> {
@@ -27,5 +27,21 @@ export class AuthService {
     return this.http.post<LoginResponse>(environment.apiUrl + ApiPath.LOGIN, user);
   }
 
+  logout(): void {
+    this.tokenService.removeToken();
+  }
 
+  isAuthenticated(): boolean {
+    return this.tokenService.isValidToken(this.tokenService.getToken());
+  }
+
+  hasRole(role: string): boolean {
+    const token = this.tokenService.getToken()
+    if (this.isAuthenticated() && token) {
+      const decodedToken = this.tokenService.decodeToken(token);
+      return decodedToken.role === role;
+    }
+    return false;
+  }
+ 
 }
